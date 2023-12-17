c6c41210966393ff74367692e0259782ae641b96
rkumae96 <154041329+rkumae96@users.noreply.github.com>
2023-12-17 12:52:41 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,30 +1,44 @@
 import torch
 from gpt2_model import GPT2, GPT2Config
 
-def load_checkpoint(filepath):
+def load_checkpoint(filepath, model):
     # Load the checkpoint
-    checkpoint = torch.load(filepath, map_location='cpu')
-    return checkpoint
+    checkpoint = torch.load(filepath, map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint)
 
 def main():
     # Initialize configuration and model
     config = GPT2Config()
-    model = GPT2(config).to('cpu')
+    model = GPT2(config)
 
     # Load the original GPT-2 125M model checkpoints
-    checkpoint = load_checkpoint("path_to_gpt2_checkpoint.pth")
-    model.load_state_dict(checkpoint)
+    checkpoint_path = "path_to_gpt2_checkpoint.pth"  # Update this path to your checkpoint file
+    load_checkpoint(checkpoint_path, model)
 
-    # Prepare input data. Here we need real input handling with tokenization and padding
-    # For demonstration purposes, we use dummy data
-    input_ids = torch.tensor([[50256] * config.max_position_embeddings])  # Example token IDs
-    mask = torch.ones_like(input_ids).unsqueeze(1).unsqueeze(2)  # Dummy attention mask
+    # Prepare input data manually (e.g., using a dictionary that maps tokens to IDs)
+    # For demonstration purposes, let's assume the following tokens and IDs:
+    tokenizer = {'The': 1212, 'quick': 12345, 'brown': 54321, ... , '<|endoftext|>': 50256}
 
-    # Forward pass through the model
+    # Encode a sample input text
+    input_text = "The quick brown fox"
+    input_ids = [tokenizer.get(word, tokenizer['<|endoftext|>']) for word in input_text.split()]
+    input_ids = torch.tensor(input_ids, dtype=torch.long).unsqueeze(0)  # Add batch dimension
+
+    # Set model to eval mode and run prediction
+    model.eval()
     with torch.no_grad():
-        predictions = model(input_ids, mask)
+        outputs = model(input_ids)
+        predictions = outputs[0]  # Get the last hidden states
 
-    print(predictions.shape)  # Expected shape: [batch_size, seq_length, vocab_size]
+    # Get the predicted token ID (greedy decoding for simplicity)
+    predicted_token_id = torch.argmax(predictions[:, -1, :], dim=-1).item()
+    
+    # Assuming you have a reverse tokenizer (ID to token)
+    reverse_tokenizer = {1212: 'The', 12345: 'quick', 54321: 'brown', ... , 50256: '<|endoftext|>'}
+    predicted_token = reverse_tokenizer.get(predicted_token_id, '<|unknown|>')
+
+    print(f"Input text: {input_text}")
+    print(f"Predicted token: {predicted_token}")
 
 if __name__ == '__main__':
     main()
